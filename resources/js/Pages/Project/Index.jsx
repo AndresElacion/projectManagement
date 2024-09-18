c3f632e1b3dad390380fabd216eeca9776511395
AndresElacion <andreselacioniii@gmail.com>
2024-09-18 11:26:49 +0800

    added destroy

--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -41,6 +41,13 @@ export default function Index({auth, projects, queryParams = null, success }) {
         router.get(route("project.index", queryParams))
     }
 
+    const deleteProject = (project) => {
+        if (!window.confirm('Are you sure you want to delete the project?')) {
+            return;
+        }
+        router.delete(route('project.destroy', project.id))
+    }
+
     return (
         <AuthenticatedLayout user={auth.user} header={
             <div className="flex justify-between items-center">
@@ -164,9 +171,11 @@ export default function Index({auth, projects, queryParams = null, success }) {
                                             <Link href={route('project.edit', project.id)} className="font-medium text-blue-600 dark:text-blue-500 hover:underline mx-1">
                                                 Edit
                                             </Link>
-                                            <Link href={route('project.destroy', project.id)} className="font-medium text-red-600 dark:text-red-500 hover:underline mx-1">
+                                            <button
+                                                onClick={e => deleteProject(project)}
+                                                className="font-medium text-red-600 dark:text-red-500 hover:underline mx-1">
                                                 Delete
-                                            </Link>
+                                            </button>
                                         </td>
                                     </tr>
                                     ))}
