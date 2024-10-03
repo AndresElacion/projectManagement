302a9431cf53d561ce68bb9fae5e22ba16b142a2
AndresElacion <andreselacioniii@gmail.com>
2024-10-03 22:28:08 +0800

    fixes

--- resources/js/constants.js
+++ resources/js/constants.js
@@ -10,6 +10,18 @@ export const PROJECT_STATUS_CLASS_MAP = {
     completed: "Completed",
   };
 
+  export const USER_STATUS_CLASS_MAP = {
+    pending: "bg-amber-500",
+    in_progress: "bg-blue-500",
+    completed: "bg-green-500",
+  };
+
+  export const USER_STATUS_TEXT_MAP = {
+    pending: "Pending",
+    in_progress: "In Progress",
+    completed: "Completed",
+  };
+
   export const TASK_STATUS_CLASS_MAP = {
     pending: "bg-amber-500",
     in_progress: "bg-blue-500",
